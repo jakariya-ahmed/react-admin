88cf7ec82bdd376e12fae6a725d6b0e446794839
jek9356 <devjakariya9356@gmail.com>
2025-11-02 22:17:02 +0600

    re-practise image & gallery image

--- src/component/product/GalleryImages.jsx
+++ src/component/product/GalleryImages.jsx
@@ -2,24 +2,22 @@ import { X } from "lucide-react";
 import { useState } from "react";
 
 export default function GalleryImages() {
-    
-    const [gallery, setGallery] = useState([]);
+  const [gallery, setGallery] = useState([]);
 
-    // Handle Gallery Chnages 
-    const handleGalleryChange = (e) => {
-        const files = Array.from(e.target.files);
-        const newImgs = files.map((file) => ({
-             file,
-            preview: URL.createObjectURL(file),
-            id: Math.random().toString(36).substr(2, 9),
-        }));
-        setGallery((prev) => [...prev, ...newImgs]);
-    }
+  const handleGalleryChange = (e) => {
+    const files = Array.from(e.target.files);
+    const newImgs = files.map((file) => ({
+      file,
+      preview: URL.createObjectURL(file),
+      id: Math.random().toString(30).substr(1,9),
+    }));
+    setGallery((prev) => [...prev, ...newImgs]);
+  } 
 
-    // Handle Remove Images
-    const handleRemove = (id) => {
-        setGallery((prev) => prev.filter((img) => img.id !== id));
-    };
+  // Handle remove images
+  const handleRemove = (id) => {
+    setGallery((prev) => prev.filter((img) => img.id !== id));
+  } 
 
 
     return (
@@ -28,57 +26,40 @@ export default function GalleryImages() {
 
             {/* Upload Images  */}
 
-            <div className="fex items-center justify-center fex-col border-1 border-dashed border-gray-400 bg-indigo-50 hover:bg-indigo-100 transition-all duration-150 rounded-md p-4">
-                <div>
-                    <input type="file" name="images/*" multiple 
-                onChange={handleGalleryChange}
-                className="h-full w-full inset-0 opacity-0 cursor-pointer" />
-                </div>
-                <div>
-                    <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-10 w-10 mb-2 text-purple-900"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={1.5}
-                    >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M3 8.25l7.5 7.5L21 8.25"
-                    />
-                    </svg>
-                    <p className="text-sm font-medium text-indigo-900">
-                    Click to upload image
-                    </p>
-                    <p className="text-xs text-gray-400">PNG, JPG, JPEG (max 5MB)</p>
-                </div>
-            </div>
-
-            {/* Images Preview  */}
-            {gallery.length > 0 && (
-                <div className="mt-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3">
-                    {gallery.map((img) => (
-            <div
-              key={img.id}
-              className="relative group rounded-lg overflow-hidden border border-gray-300"
+      <label className="cursor-pointer flex flex-col items-center justify-center w-full h-40 border-2 border-dashed border-gray-300 rounded-lg bg-indigo-100 trasition-all duration-150">
+        <div className="flex flex-col items-center text-gray-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-10 w-10 mb-2 text-purple-900"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={1.5}
             >
-              <img
-                src={img.preview}
-                alt="preview"
-                className="w-full"
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M3 8.25l7.5 7.5L21 8.25"
               />
-              <button
+            </svg>
+            <p className="text-sm font-medium text-indigo-900"> Click to Upload Images </p>
+            <p className="text-xs text-gray-400">PNG, JPG, JPEG, (MAX 5MB)</p>
+        </div>
+        <input type="file" accept="image/*" multiple className="hidden" onChange={handleGalleryChange}/>
+      </label>
+      {/* Preview Images  */}
+      <div className="mt-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3">
+        {gallery.map((img) => (
+          <div key={img.id} className="relative group rounded-lg overflow-hidden border border-gray-300">
+              <img src={img.preview} alt="preview" className="w-full" />
+              {/* remove icon  */}
+              <button 
                 onClick={() => handleRemove(img.id)}
                 className="absolute top-1 right-1 bg-red-600 text-white rounded-full p-1 opacity-0 group-hover:opacity-100 transition cursor-pointer"
-              >
-                <X size={16} />
-              </button>
-            </div>
-          ))}
-                </div>
-            )}
+              ><X size={16} /></button>
+          </div>
+        ))}
+      </div>
 
         </div>
     );
